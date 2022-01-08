103852d51c35e8cba8f8ceecc77d7dea989d44c3
lucrox <ludovic.lagay@gmail.com>
2022-01-09 00:38:30 +0100

    constructeurs

--- gloo.numberlink1/src/gloo/numberlink1/Controller.java
+++ gloo.numberlink1/src/gloo/numberlink1/Controller.java
@@ -2,9 +2,15 @@ package gloo.numberlink1;
 
 public class Controller {
 	private Grid grid;
+	private Path currentPath;
+	
 	
 	public Controller(Grid grid) {
 		this.grid = grid;
+	}
+	
+	public void setCurrentPath(Path currentPath) {
+		this.currentPath = currentPath;
 	}
 
 }
--- gloo.numberlink1/src/gloo/numberlink1/Path.java
+++ gloo.numberlink1/src/gloo/numberlink1/Path.java
@@ -2,5 +2,10 @@ package gloo.numberlink1;
 
 public class Path {
 	private Cell[] cells;
+	private Tag tag;
+	
+	public Path(Tag tag) {
+		this.tag = tag;
+	}
 
 }
