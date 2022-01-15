ea22dd536495d87201db317f0b4a4e186b713a75
Pinglei He <pingleihe@outlook.com>
2022-01-15 18:20:36 +0100

    added invalid parameter exception

---
+++ src/gloo/numberlink/exception/InvalidParametersException.java
@@ -0,0 +1,7 @@
+package gloo.numberlink.exception;
+
+public class InvalidParametersException extends Exception {
+    public InvalidParametersException(String errorMessage) {
+        super(errorMessage);
+    }
+}
